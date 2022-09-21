8053ce543747e8edf0c162556b493dec61a0d1c5
Ravi verma <support30@anaxee.com>
2022-09-21 17:24:52 +0530

    add functionality

---
+++ src/FormBuilder.js
@@ -0,0 +1,34 @@
+import $ from "jquery";
+import React, {Component, createRef, useEffect} from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+// import 'jquery-ui-sortable';
+// import 'jquery-ui-draggable';
+// import 'jquery-ui-droppable';
+
+
+window.jQuery = $;
+window.$ = $;
+//
+require("formBuilder");
+require("jquery-ui-sortable");
+
+function FormBuilder() {
+  const fb = createRef();
+  const fr = createRef();
+  const formBuilderOptions = {
+    
+  };
+
+  useEffect(() => {
+    $(fb.current).formBuilder(formBuilderOptions);
+  }, []);
+
+
+  return (
+          <div ref={fb} />
+  );
+  
+}
+
+export default FormBuilder;
---
+++ src/save.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Save(props) {
+  return (
+    <div>{props.id}</div>
+  )
+}
